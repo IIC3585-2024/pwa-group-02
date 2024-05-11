473a2a7ab2e8b9cd57ef08a8c7d5769d25135b81
PSepulvedaS <pablo.seplveda@uc.cl>
2024-05-11 17:53:31 -0400

    Added Firebase

---
+++ back/server.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGGgJDjB4_jeXk5xyTwIaIGCamVnTdfDM",
+  authDomain: "play-planner-683e3.firebaseapp.com",
+  projectId: "play-planner-683e3",
+  storageBucket: "play-planner-683e3.appspot.com",
+  messagingSenderId: "15830657868",
+  appId: "1:15830657868:web:8fc7a0bcacc7d1e4dffde9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
