aff42fc8c5cf68b46f87ca06152486f54654c842
Gabriel Quiroz <gabriel.quiroz@uc.cl>
2024-05-12 15:17:06 -0400

    Fix: caching on sw install

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const staticCacheName = 'static-play-planner';
 const staticAssets = [
-    '/pwa-group-02/',
-    '/pwa-group-02/index.html',
-    '/pwa-group-02/script.js',
-    '/pwa-group-02/styles/styles.css',
+    './pwa-group-02/',
+    './pwa-group-02/index.html',
+    './pwa-group-02/script.js',
+    './pwa-group-02/styles/styles.css',
 ];
 
 self.addEventListener('install', event => {
@@ -15,28 +15,28 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-    const { request } = event;
-    const url = new URL(request.url);
-    if (url.origin === location.origin) {
-        event.respondWith(cacheData(request));
-    } else {
-        event.respondWith(networkFirst(request));
-    }
-});
+// self.addEventListener('fetch', event => {
+//     const { request } = event;
+//     const url = new URL(request.url);
+//     if (url.origin === location.origin) {
+//         event.respondWith(cacheData(request));
+//     } else {
+//         event.respondWith(networkFirst(request));
+//     }
+// });
 
-async function cacheData(request) {
-    const cachedResponse = await caches.match(request);
-    return cachedResponse || fetch(request);
-}
+// async function cacheData(request) {
+//     const cachedResponse = await caches.match(request);
+//     return cachedResponse || fetch(request);
+// }
 
-async function networkFirst(request) {
-    const cache = await caches.open('dynamic-play-planner');
-    try {
-        const response = await fetch(request);
-        cache.put(request, response.clone());
-        return response;
-    } catch (error){
-        return await cache.match(request);
-    }
-}
+// async function networkFirst(request) {
+//     const cache = await caches.open('dynamic-play-planner');
+//     try {
+//         const response = await fetch(request);
+//         cache.put(request, response.clone());
+//         return response;
+//     } catch (error){
+//         return await cache.match(request);
+//     }
+// }
