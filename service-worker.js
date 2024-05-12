71f9e746084af184c0cd6ea3369f65873fafc4c4
Gabriel Quiroz <gabriel.quiroz@uc.cl>
2024-05-12 15:41:42 -0400

    Fix: sw fetch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
+// Código basado en: https://www.youtube.com/playlist?list=PL4cUxeGkcC9gTxqJBcDmoi5Q2pzDusSL7
+
 const staticCacheName = 'static-play-planner';
+const dynamicCacheName = 'dynamic-play-planner';
 const staticAssets = [
     './',
     './index.html',
@@ -15,28 +18,28 @@ self.addEventListener('install', event => {
   );
 });
 
-// self.addEventListener('fetch', event => {
-//     const { request } = event;
-//     const url = new URL(request.url);
-//     if (url.origin === location.origin) {
-//         event.respondWith(cacheData(request));
-//     } else {
-//         event.respondWith(networkFirst(request));
-//     }
-// });
+self.addEventListener('fetch', event => {
+    const { request } = event;
+    const url = new URL(request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheData(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
+});
 
-// async function cacheData(request) {
-//     const cachedResponse = await caches.match(request);
-//     return cachedResponse || fetch(request);
-// }
+async function cacheData(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}
 
-// async function networkFirst(request) {
-//     const cache = await caches.open('dynamic-play-planner');
-//     try {
-//         const response = await fetch(request);
-//         cache.put(request, response.clone());
-//         return response;
-//     } catch (error){
-//         return await cache.match(request);
-//     }
-// }
+async function networkFirst(request) {
+    const cache = await caches.open(dynamicCacheName);
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    } catch (error){
+        return await cache.match(request);
+    }
+}
